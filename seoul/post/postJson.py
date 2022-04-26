c2ebd29b57c1a8051c71b5c59975a62304a1bf9e
garamminchan <garamminchan@gmail.com>
2022-04-26 20:35:29 +0900

    chrome option added

--- seoul/post/postJson.py
+++ seoul/post/postJson.py
@@ -5,6 +5,4 @@ url = "http://ec2-13-124-189-245.ap-northeast-2.compute.amazonaws.com:8080/court
 
 
 def post_json(json_data):
-    print(json_data)
     response = requests.post(url, data=json.dumps(json_data), headers ={'content-type' : 'application/json'})
-    print(response)
